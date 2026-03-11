8d9ead2aea6582f963e22a1fcd2d55c176126f6d
Vishal Raghav <vishalraghav321@gmail.com>
2026-03-11 09:22:29 +0530

    Created a basic java program

---
+++ hello.java
@@ -0,0 +1,13 @@
+class Clock {
+    void display() {
+        System.err.println("Hello");
+    }
+};
+
+public class hello {
+    public static void main(String[] args) {
+
+        Clock c1 = new Clock();
+        c1.display();
+    }
+}
